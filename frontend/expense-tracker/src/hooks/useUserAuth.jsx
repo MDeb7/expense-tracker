7e5566fab5843bc89ae627dca0e920a789c4fb35
Debjyoti <majumderdebjyoti8@example.com>
2025-05-24 17:52:34 +0530

    fifth commit

--- frontend/expense-tracker/src/hooks/useUserAuth.jsx
+++ frontend/expense-tracker/src/hooks/useUserAuth.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { useEffect } from "react";
-import { UserContext } from "../context/UserContext";
+import { UserContext } from "../context/userContext";
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "../utils/axiosInstance";
 import { API_PATHS } from "../utils/apiPaths";
